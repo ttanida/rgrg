89a97593450c58f3ebc0e22f19f77856fa9ec202
Tim Tanida <tanida.tim@googlemail.com>
2022-05-29 17:01:45 +0000

    Add requirements.txt and add create_dataset.csv.py

---
+++ src/dataset/create_dataset_csv.py
@@ -0,0 +1,12 @@
+import os
+
+path_to_chest_imagenome = "/u/home/tanida/datasets/chest-imagenome-dataset"
+path_to_mimic_cxr = "/u/home/tanida/datasets/mimic-cxr-jpg"
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
