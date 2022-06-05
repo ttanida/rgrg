b3cf300c36ea97277632d8d813268c79b97318e0
Tim Tanida <tanida.tim@googlemail.com>
2022-06-05 21:38:29 +0000

    Format compute_stats_dataset.py

--- src/dataset/compute_stats_dataset.py
+++ src/dataset/compute_stats_dataset.py
@@ -1,7 +1,6 @@
 from collections import defaultdict
 import csv
 import json
-import logging
 import os
 
 from tqdm import tqdm
@@ -10,11 +9,14 @@ from constants import ANATOMICAL_REGIONS, IMAGE_IDS_TO_IGNORE
 
 path_to_chest_imagenome = "/u/home/tanida/datasets/chest-imagenome-dataset"
 
-logging.basicConfig(level=logging.INFO, format="[%(levelname)s]: %(message)s")
-log = logging.getLogger(__name__)
+
+def print_stats_counter_dicts(counter_dict):
+    total_count = sum(value for value in counter_dict.values())
+    for bbox_name, count in sorted(counter_dict.items(), key=lambda k_v: k_v[1], reverse=True):
+        print(f"\t\t{bbox_name}: {count:,} ({(count/total_count) * 100:.2f}%)")
 
 
-def print_stats(dataset, stats):
+def print_stats(dataset: str, stats: tuple) -> None:
     num_images, num_ignored_images, num_bboxes, num_bboxes_with_phrases, num_outlier_bboxes, bbox_with_phrases_counter_dict, outlier_bbox_counter_dict = stats
 
     print(f"\n{dataset}:")
@@ -29,18 +31,19 @@ def print_stats(dataset, stats):
     print(f"\t-> {(num_outlier_bboxes/num_bboxes_with_phrases) * 100:.2f}% of bboxes with phrases have 'outlier' names")
 
     print("\n\tCounts and percentages of 'outlier' bboxes:")
-    total_count = sum(value for value in outlier_bbox_counter_dict.values())
-    for bbox_name, count in sorted(outlier_bbox_counter_dict.items(), key=lambda k_v: k_v[1], reverse=True):
-        print(f"\t\t{bbox_name}: {count:,} ({(count/total_count) * 100:.2f}%)")
+    print_stats_counter_dicts(outlier_bbox_counter_dict)
 
     print("\n\tCounts and percentages of normal bboxes with phrases:")
-    total_count = sum(value for value in bbox_with_phrases_counter_dict.values())
-    for bbox_name, count in sorted(bbox_with_phrases_counter_dict.items(), key=lambda k_v: k_v[1], reverse=True):
-        print(f"\t\t{bbox_name}: {count:,} ({(count/total_count) * 100:.2f}%)")
+    print_stats_counter_dicts(bbox_with_phrases_counter_dict)
+
     print()
 
 
-def get_stats_image(image_scene_graph: dict, num_outlier_bboxes: int, num_bboxes_with_phrases: int, bbox_counter_dict: dict, outlier_bbox_counter_dict: dict) -> tuple[int, int]:
+def get_stats_image(image_scene_graph: dict,
+                    num_outlier_bboxes: int,
+                    num_bboxes_with_phrases: int,
+                    bbox_counter_dict: dict,
+                    outlier_bbox_counter_dict: dict) -> tuple[int, int]:
     for attribute in image_scene_graph["attributes"]:
         bbox_name = attribute["bbox_name"]
         num_bboxes_with_phrases += 1
@@ -66,7 +69,7 @@ def get_num_rows(path_csv_file: str) -> int:
         return sum(1 for row in csv_reader)
 
 
-def compute_stats_for_csv_file(dataset, path_csv_file):
+def compute_stats_for_csv_file(dataset: str, path_csv_file: str) -> tuple:
     num_images = get_num_rows(path_csv_file)
     num_ignored_images = 0  # images that are ignored because they are faulty (due to faulty x-rays etc.)
 
@@ -75,7 +78,7 @@ def compute_stats_for_csv_file(dataset, path_csv_file):
     num_outlier_bboxes = 0  # bboxes that have bbox names (like 'left breast' etc.) that are not in the 36 anatomical regions are considered outliers
 
     bbox_with_phrases_counter_dict = defaultdict(int)  # dict to count how often each of the 36 anatomical regions have phrases
-    outlier_bbox_counter_dict = defaultdict(int) # dict to count how often each of the outlier regions have phrases
+    outlier_bbox_counter_dict = defaultdict(int)  # dict to count how often each of the outlier regions have phrases
 
     with open(path_csv_file) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=",")
@@ -98,13 +101,15 @@ def compute_stats_for_csv_file(dataset, path_csv_file):
             with open(chest_imagenome_scene_graph_file_path) as fp:
                 image_scene_graph = json.load(fp)
 
+            # compute new number for num_outlier_bboxes and num_bboxes_with_phrases based on current image
+            # also increase the counter dicts
             num_outlier_bboxes, num_bboxes_with_phrases = get_stats_image(image_scene_graph, num_outlier_bboxes, num_bboxes_with_phrases, bbox_with_phrases_counter_dict, outlier_bbox_counter_dict)
 
             # for each image, there are 36 bboxes for 36 anatomical regions
             num_bboxes += 36
 
     stats = num_images, num_ignored_images, num_bboxes, num_bboxes_with_phrases, num_outlier_bboxes, bbox_with_phrases_counter_dict, outlier_bbox_counter_dict
-    print_stats(dataset, stats)
+    print_stats(dataset=dataset, stats=stats)
 
     return stats
 
@@ -122,10 +127,18 @@ def compute_and_print_stats_for_csv_files(csv_files_dict):
 
     for dataset, path_csv_file in csv_files_dict.items():
         stats = compute_stats_for_csv_file(dataset, path_csv_file)
-        num_images, num_ignored_images, num_bboxes, num_bboxes_with_phrases, num_outlier_bboxes, bbox_with_phrases_counter_dict, outlier_bbox_counter_dict = stats
+
+        num_images, \
+            num_ignored_images, \
+            num_bboxes, \
+            num_bboxes_with_phrases, \
+            num_outlier_bboxes, \
+            bbox_with_phrases_counter_dict, \
+            outlier_bbox_counter_dict = stats
 
         total_num_images += num_images
         total_num_ignored_images += num_ignored_images
+
         total_num_bboxes += num_bboxes
         total_num_bboxes_with_phrases += num_bboxes_with_phrases
         total_num_outlier_bboxes += num_outlier_bboxes
@@ -136,8 +149,15 @@ def compute_and_print_stats_for_csv_files(csv_files_dict):
         for key, value in outlier_bbox_counter_dict.items():
             total_outlier_bbox_counter_dict[key] += value
 
-    stats = total_num_images, total_num_ignored_images, total_num_bboxes, total_num_bboxes_with_phrases, total_num_outlier_bboxes, total_bbox_with_phrases_counter_dict, total_outlier_bbox_counter_dict
-    print_stats("Total", stats)
+    stats = total_num_images, \
+        total_num_ignored_images, \
+        total_num_bboxes, \
+        total_num_bboxes_with_phrases, \
+        total_num_outlier_bboxes, \
+        total_bbox_with_phrases_counter_dict, \
+        total_outlier_bbox_counter_dict
+
+    print_stats(dataset="Total", stats=stats)
 
 
 def get_train_val_test_csv_files():
