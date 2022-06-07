d3e904d053197436a8e0c7c911f7ba816922d07c
Tim Tanida <tanida.tim@googlemail.com>
2022-06-07 09:36:18 +0000

    Finish custom_image_dataset.py

--- src/encoder/custom_image_dataset.py
+++ src/encoder/custom_image_dataset.py
@@ -1,6 +1,6 @@
+from PIL import Image
 import torch
 from torch.utils.data import Dataset
-from torchvision.io import read_image
 
 from src.dataset.constants import ANATOMICAL_REGIONS
 
@@ -15,17 +15,16 @@ class CustomImageDataset(Dataset):
         return len(self.dataset_df)
 
     def __getitem__(self, index):
-        img_path = self.dataset_df.iloc[index, 0]  # mimic_image_file_path is the 1st column of the dataframes
-        image = read_image(img_path)
+        image_path = self.dataset_df.iloc[index, 0]  # mimic_image_file_path is the 1st column of the dataframes
+        image = Image.open(image_path)
 
-        bbox_coordinates = self.dataset_df.iloc[index, 2:6].tolist()  # bbox_coordinates = [x1, y1, x2, y2]
-
-        # TODO: crop image
+        bbox_coordinates = self.dataset_df.iloc[index, 2:6].tolist()  # bbox_coordinates = [x1, y1, x2, y2], x1 and y1 are for the top left corner and x2 and y2 are for the bottom right corner
+        cropped_image = image.crop(box=bbox_coordinates)
 
         cropped_image = self.transform(cropped_image)
 
         bbox_class_index = ANATOMICAL_REGIONS[self.dataset_df.iloc[index, 1]]  # get the bbox_name (2nd column of df) and convert it into corresponding class index
         is_abnormal_int = int(self.dataset_df.iloc[index, 6])  # get the is_abnormal boolean variable (7th column of df) and convert it into integer
-
         labels = torch.Tensor([bbox_class_index, is_abnormal_int])
+
         return cropped_image, labels
