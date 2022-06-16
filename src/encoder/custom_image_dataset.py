d986c6dae87124611787d68cd53af8f4b2ac154c
Tim Tanida <tanida.tim@googlemail.com>
2022-06-16 14:51:00 +0000

    Add exception handling to __getitem__

--- src/encoder/custom_image_dataset.py
+++ src/encoder/custom_image_dataset.py
@@ -20,31 +20,37 @@ class CustomImageDataset(Dataset):
 
         # cv2.imread by default loads an image with 3 channels
         # since we have grayscale images, we only have 1 channel and thus use cv2.IMREAD_UNCHANGED to read in the 1 channel
-        image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+        try:
+            image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+        except:
+            return None
 
-        # get the coordinates of the bbox
-        # x1 and y1 are for the top left corner and x2 and y2 are for the bottom right corner
-        x1, y1, x2, y2 = self.dataset_df.iloc[index, 2:6]
+        try:
+            # get the coordinates of the bbox
+            # x1 and y1 are for the top left corner and x2 and y2 are for the bottom right corner
+            x1, y1, x2, y2 = self.dataset_df.iloc[index, 2:6]
 
-        # crop the image (which is a np array at this point)
-        cropped_image = image[y1:y2, x1:x2]  # cropped_image = image[Y:Y+H, X:X+W]
+            # crop the image (which is a np array at this point)
+            cropped_image = image[y1:y2, x1:x2]  # cropped_image = image[Y:Y+H, X:X+W]
 
-        # apply transformations
-        # albumentations transforms return a dict, which is why key "image" has to be selected
-        cropped_image = self.transforms(image=cropped_image)["image"]
+            # apply transformations
+            # albumentations transforms return a dict, which is why key "image" has to be selected
+            cropped_image = self.transforms(image=cropped_image)["image"]
 
-        # get the bbox_name (2nd column of df) and convert it into corresponding class index
-        bbox_class_index = int(ANATOMICAL_REGIONS[self.dataset_df.iloc[index, 1]])
+            # get the bbox_name (2nd column of df) and convert it into corresponding class index
+            bbox_class_index = int(ANATOMICAL_REGIONS[self.dataset_df.iloc[index, 1]])
 
-        # get the is_abnormal boolean variable (7th column of df) and convert it to a float
-        is_abnormal = float(self.dataset_df.iloc[index, 6])
+            # get the is_abnormal boolean variable (7th column of df) and convert it to a float
+            is_abnormal = float(self.dataset_df.iloc[index, 6])
 
-        sample = {
-            "image": cropped_image,
-            # pytorch's cross entropy loss expects the targets to be of dtype int
-            "bbox_target": torch.tensor(bbox_class_index, dtype=torch.uint8),
-            # pytorch's binary cross entropy loss expects the targets to be of dtype float
-            "is_abnormal_target": torch.tensor(is_abnormal, dtype=torch.float32)
-        }
+            sample = {
+                "image": cropped_image,
+                # pytorch's cross entropy loss expects the targets to be of dtype int
+                "bbox_target": torch.tensor(bbox_class_index, dtype=torch.uint8),
+                # pytorch's binary cross entropy loss expects the targets to be of dtype float
+                "is_abnormal_target": torch.tensor(is_abnormal, dtype=torch.float32)
+            }
+        except:
+            return None
 
         return sample
