79dd5fdf680bba37f87b3f3ad25afed114f3c735
Tim Tanida <tanida.tim@googlemail.com>
2022-07-24 20:15:32 +0200

    Implement transforms in training script

--- src/full_model_with_classifier_encoder/training_script_full_model.py
+++ src/full_model_with_classifier_encoder/training_script_full_model.py
@@ -415,7 +415,7 @@ def get_data_loaders(tokenizer, train_dataset_complete, val_dataset_complete):
 
 
 def get_transforms(dataset: str):
-    # see compute_mean_std_dataset.py in src/dataset
+    # see compute_mean_std_dataset.py in src/dataset_bounding_boxes
     mean = 0.471
     std = 0.302
 
--- src/object_detector/training_script_object_detector.py
+++ src/object_detector/training_script_object_detector.py
@@ -1,9 +1,14 @@
+from ast import literal_eval
 import os
 
 import logging
 import random
 
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+import cv2
 import numpy as np
+import pandas as pd
 import torch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -20,7 +25,7 @@ torch.manual_seed(seed_val)
 torch.cuda.manual_seed_all(seed_val)
 
 # define configurations for training run
-RUN = 3
+RUN = 0
 PERCENTAGE_OF_TRAIN_SET_TO_USE = 1
 PERCENTAGE_OF_VAL_SET_TO_USE = 1
 BATCH_SIZE = 16
@@ -30,6 +35,69 @@ LR = 1e-2
 EVALUATE_EVERY_K_STEPS = 3500  # how often to evaluate the model on the validation set and log metrics to tensorboard (additionally, model will always be evaluated at end of epoch)
 PATIENCE = 10  # number of evaluations to wait before early stopping
 PATIENCE_LR_SCHEDULER = 3  # number of evaluations to wait for val loss to reduce before lr is reduced by 1e-1
+
+
+def get_transforms(dataset: str):
+    # see compute_mean_std_dataset.py in src/dataset_bounding_boxes
+    mean = 0.471
+    std = 0.302
+
+    # note: transforms are applied to the already resized (to 224x224) and padded images 
+    # (see __getitem__ method of custom dataset class)!
+
+    # use albumentations for Compose and transforms
+    train_transforms = A.Compose([
+        # optionally add augmentation transforms here (but bboxes also have to be transformed in this case!)
+        A.Normalize(mean=mean, std=std),
+        ToTensorV2()
+    ])
+
+    # don't apply data augmentations to val and test set
+    val_test_transforms = A.Compose(
+        [
+            A.Normalize(mean=mean, std=std),
+            ToTensorV2(),
+        ]
+    )
+
+    if dataset == "train":
+        return train_transforms
+    else:
+        return val_test_transforms
+
+
+def get_datasets_as_dfs(config_file_path):
+    path_dataset_object_detector = "/u/home/tanida/datasets/object-detector-dataset"
+
+    usecols = ["mimic_image_file_path", "bbox_coordinates", "labels"]
+
+    # since bbox_coordinates and labels are stored as strings in the csv_file, we have to apply 
+    # the literal_eval func to convert them to python lists
+    converters = {"bbox_coordinates": literal_eval, "labels": literal_eval}
+
+    datasets_as_dfs = {dataset: os.path.join(path_dataset_object_detector, f"{dataset}-50") + ".csv" for dataset in ["train", "valid", "test"]}
+    datasets_as_dfs = {dataset: pd.read_csv(csv_file_path, usecols=usecols, converters=converters) for dataset, csv_file_path in datasets_as_dfs.items()}
+
+    total_num_samples_train = len(datasets_as_dfs["train"])
+    total_num_samples_val = len(datasets_as_dfs["valid"])
+
+    # compute new number of samples for both train and val
+    new_num_samples_train = int(PERCENTAGE_OF_TRAIN_SET_TO_USE * total_num_samples_train)
+    new_num_samples_val = int(PERCENTAGE_OF_VAL_SET_TO_USE * total_num_samples_val)
+
+    log.info(f"Train: {new_num_samples_train} phrases")
+    log.info(f"Val: {new_num_samples_val} phrases")
+
+    with open(config_file_path, "a") as f:
+        f.write(f"\tTRAIN NUM PHRASES: {new_num_samples_train}\n")
+        f.write(f"\tVAL NUM PHRASES: {new_num_samples_val}\n")
+
+    # limit the datasets to those new numbers
+    datasets_as_dfs["train"] = datasets_as_dfs["train"][:new_num_samples_train]
+    datasets_as_dfs["valid"] = datasets_as_dfs["valid"][:new_num_samples_val]
+
+    return datasets_as_dfs
+
 
 def create_run_folder():
     """
@@ -76,6 +144,14 @@ def create_run_folder():
 
 def main():
     weights_folder_path, tensorboard_folder_path, config_file_path = create_run_folder()
+
+    datasets_as_dfs = get_datasets_as_dfs(config_file_path)
+
+    train_transforms = get_transforms("train")
+    val_transforms = get_transforms("val")
+
+    train_dataset = pass
+    val_dataset = pass
 
 if __name__ == "__main__":
     main()
