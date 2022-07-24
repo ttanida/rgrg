e7c81133769fda5263eaf43c12961e0970a6bb7e
Tim Tanida <tanida.tim@googlemail.com>
2022-07-24 16:43:07 +0200

    Continue implementing forward function, create ImageList class

---
+++ src/object_detector/image_list.py
@@ -0,0 +1,24 @@
+import torch
+from torch import Tensor
+
+
+class ImageList:
+    """
+    rpn and roi_heads of object detector require images to be of custom type ImageList.
+
+    This class is a slightly modified implementation of the PyTorch implementation.
+    (https://github.com/pytorch/vision/blob/main/torchvision/models/detection/image_list.py)
+    """
+
+    def __init__(self, images_tensor: Tensor) -> None:
+        self.images_tensor = images_tensor
+
+        # all tensors have the same shape (most likely [batch_size, 1, 224, 224])
+        batch_size = images_tensor.shape[0]
+        image_sizes = images_tensor.shape[-2:]
+
+        self.image_sizes = [tuple(image_sizes) for _ in range(batch_size)]
+
+    def to(self, device: torch.device) -> "ImageList":
+        cast_tensor = self.images_tensor.to(device)
+        return ImageList(cast_tensor, self.image_sizes)
--- src/object_detector/object_detector.py
+++ src/object_detector/object_detector.py
@@ -1,14 +1,17 @@
+from collections import OrderedDict
 from typing import Optional, List, Dict
 
 import torch
+from torch import Tensor
 import torch.nn as nn
 import torchvision
-from torchvision.models.detection import FasterRCNN
 from torchvision.models.detection.faster_rcnn import TwoMLPHead, FastRCNNPredictor
 from torchvision.models.detection.roi_heads import RoIHeads
 from torchvision.models.detection.rpn import AnchorGenerator, RPNHead, RegionProposalNetwork
 from torchinfo import summary
 import torchxrayvision as xrv
+
+from image_list import ImageList
 
 
 class ObjectDetector(nn.Module):
@@ -51,14 +54,6 @@ class ObjectDetector(nn.Module):
         self.rpn = self._create_rpn()
         self.roi_heads = self._create_roi_heads()
 
-        # put the pieces together inside a FasterRCNN model
-        self.model = FasterRCNN(
-            backbone=self.backbone,
-            num_classes=self.num_classes,
-            rpn_anchor_generator=self.anchor_generator,
-            box_roi_pool=self.roi_pooler
-        )
-
     def _create_rpn(self):
         # since we have 36 anatomical regions of varying sizes and aspect ratios,
         # we have to define a custom anchor generator that generates anchors that suit
@@ -72,7 +67,7 @@ class ObjectDetector(nn.Module):
         # since the input image size is 224 x 224, we choose the sizes accordingly
         anchor_generator = AnchorGenerator(
             sizes=((10, 20, 30, 40, 50, 60, 70, 80, 90, 150),),
-            aspect_ratios=((0.2, 0.25, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.3, 1.5, 2.1, 2.6, 3.0, 8.0),)
+            aspect_ratios=((0.2, 0.25, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.3, 1.5, 2.1, 2.6, 3.0, 8.0),),
         )
 
         rpn_head = RPNHead(self.backbone.out_channels, anchor_generator.num_anchors_per_location())
@@ -88,7 +83,7 @@ class ObjectDetector(nn.Module):
             pre_nms_top_n={"training": 2000, "testing": 1000},
             post_nms_top_n={"training": 2000, "testing": 1000},
             nms_thresh=0.7,
-            score_thresh=0.0
+            score_thresh=0.0,
         )
 
         return rpn
@@ -97,16 +92,12 @@ class ObjectDetector(nn.Module):
         # define the roi pooling layer
         # if the backbone returns a Tensor, featmap_names is expected to be [0]
         # (uniform) size of feature maps after roi pooling layer is defined in output_size
-        roi_pooler = torchvision.ops.MultiScaleRoIAlign(
-            featmap_names=["0"],
-            output_size=7,
-            sampling_ratio=2
-        )
+        roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names=["0"], output_size=7, sampling_ratio=2)
 
         resolution = roi_pooler.output_size[0]
         representation_size = 1024
 
-        box_head = TwoMLPHead(self.backbone.out_channels * resolution ** 2, representation_size)
+        box_head = TwoMLPHead(self.backbone.out_channels * resolution**2, representation_size)
         box_predictor = FastRCNNPredictor(representation_size, self.num_classes)
 
         # use default values for RoI heads
@@ -121,15 +112,100 @@ class ObjectDetector(nn.Module):
             bbox_reg_weights=None,
             score_thresh=0.05,
             nms_thresh=0.5,
-            detections_per_img=100
+            detections_per_img=100,
         )
 
         return roi_heads
 
+    def _check_targets(self, targets):
+        """
+        Check if
+            - there are targets for training
+            - all bboxes are of correct type and shape
+            - there are no degenerate bboxes (where e.g. x1 > x2)
+        """
+        if targets is None:
+            torch._assert(False, "targets should not be none when in training mode")
+
+        for target_idx, target in enumerate(targets):
+            boxes = target["boxes"]
+            if not isinstance(boxes, torch.Tensor):
+                torch._assert(False, f"Expected target boxes to be of type Tensor, got {type(boxes)}.")
+
+            torch._assert(
+                len(boxes.shape) == 2 and boxes.shape[-1] == 4,
+                f"Expected target boxes to be a tensor of shape [N, 4], got {boxes.shape}.",
+            )
+
+            # x1 should always be < x2 and y1 should always be < y2
+            degenerate_boxes = boxes[:, 2:] <= boxes[:, :2]
+            if degenerate_boxes.any():
+                # print the first degenerate box
+                bb_idx = torch.where(degenerate_boxes.any(dim=1))[0][0]
+                degen_bb: List[float] = boxes[bb_idx].tolist()
+                torch._assert(
+                    False,
+                    "All bounding boxes should have positive height and width."
+                    f" Found invalid box {degen_bb} for target at index {target_idx}.",
+                )
+
+    def _transform_inputs_for_rpn_and_roi(self, images, features):
+        """
+        Tranform images and features from tensors to types that the rpn and roi_heads expect in the current PyTorch implementation.
+
+        Concretely, images have to be of class ImageList, which is a custom PyTorch class.
+        Features have to be a dict where the str "0" maps to the features.
+
+        Args:
+            images (Tensor)
+            features (Tensor): of shape [batch_size, 1024, 7, 7]
+
+        Returns:
+            images (ImageList)
+            features (Dict[str, Tensor])
+        """
+        images = ImageList(images)
+        features = OrderedDict([("0", features)])
+
+        return images, features
+
     def forward(self,
-                images: List[torch.Tensor],  # single image is of shape (1 x 224 x 224) (gray-scale images of size 224 x 224)
-                targets: Optional[List[Dict[str, torch.Tensor]]] = None):  # single target is a dict containing "boxes" and "labels" keys
-        return self.model.forward(images, targets)
+                images: Tensor,
+                targets: Optional[List[Dict[str, Tensor]]] = None):
+        """
+        Args:
+            images (Tensor): images to be processed of shape [batch_size, 1, 224, 224] (gray-scale images of size 224 x 224)
+            targets (List[Dict[str, Tensor]]): list of dicts, where a single dict contains the fields:
+                - boxes (FloatTensor[N, 4]): the ground-truth boxes in [x1, y1, x2, y2] format
+                - labels (Int64Tensor[N]): the class label for each ground-truth box
+
+        Returns:
+            during training: losses (Dict[Tensor]), which contains the 4 losses
+
+            during inference: detections (List[Dict[str, Tensor]]), the predictions for each input image.
+            The fields of a single dict are:
+                - boxes (FloatTensor[N, 4]): the predicted boxes in [x1, y1, x2, y2] format
+                - labels (Int64Tensor[N]): the predicted labels for each image
+                - scores (Tensor[N]): the scores or each prediction
+        """
+        if self.training:
+            self._check_targets(targets)
+
+        features = self.backbone(images)
+
+        images, features = self._transform_inputs_for_rpn_and_roi(images, features)
+
+        proposals, proposal_losses = self.rpn(images, features, targets)
+        detections, detector_losses = self.roi_heads(features, proposals, images.image_sizes, targets)
+
+        if self.training:
+            losses = {}
+            losses.update(detector_losses)
+            losses.update(proposal_losses)
+
+            return losses
+        else:
+            return detections
 
 
 device = torch.device("cpu")
