b47f266bbde35d841fd8c9ac27050cd4a8e48d56
Tim Tanida <tanida.tim@googlemail.com>
2022-07-28 23:31:34 +0200

    Add postprocess_detections method to custom_roi_heads class

--- src/object_detector/custom_roi_heads.py
+++ src/object_detector/custom_roi_heads.py
@@ -2,7 +2,9 @@ from typing import Optional, List, Dict, Tuple
 
 import torch
 from torch import Tensor
+import torch.nn.functional as F
 from torchvision.models.detection.roi_heads import RoIHeads, fastrcnn_loss
+from torchvision.ops import boxes as box_ops
 
 
 class CustomRoIHeads(RoIHeads):
@@ -50,6 +52,64 @@ class CustomRoIHeads(RoIHeads):
             keypoint_predictor,
         )
         self.return_feature_vectors = return_feature_vectors
+
+    def postprocess_detections(
+        self,
+        class_logits: Tensor,
+        box_regression: Tensor,
+        proposals: List[Tensor],
+        image_shapes: List[Tuple[int, int]]
+    ) -> Tuple[List[Tensor], List[Tensor], List[Tensor]]:
+        device = class_logits.device
+        num_classes = class_logits.shape[-1]
+
+        boxes_per_image = [boxes_in_image.shape[0] for boxes_in_image in proposals]
+        pred_boxes = self.box_coder.decode(box_regression, proposals)
+
+        pred_scores = F.softmax(class_logits, -1)
+
+        pred_boxes_list = pred_boxes.split(boxes_per_image, 0)
+        pred_scores_list = pred_scores.split(boxes_per_image, 0)
+
+        all_boxes = []
+        all_scores = []
+        all_labels = []
+        for boxes, scores, image_shape in zip(pred_boxes_list, pred_scores_list, image_shapes):
+            boxes = box_ops.clip_boxes_to_image(boxes, image_shape)
+
+            # create labels for each prediction
+            labels = torch.arange(num_classes, device=device)
+            labels = labels.view(1, -1).expand_as(scores)
+
+            # remove predictions with the background label
+            boxes = boxes[:, 1:]
+            scores = scores[:, 1:]
+            labels = labels[:, 1:]
+
+            # batch everything, by making every class prediction be a separate instance
+            boxes = boxes.reshape(-1, 4)
+            scores = scores.reshape(-1)
+            labels = labels.reshape(-1)
+
+            # remove low scoring boxes
+            inds = torch.where(scores > self.score_thresh)[0]
+            boxes, scores, labels = boxes[inds], scores[inds], labels[inds]
+
+            # remove empty boxes
+            keep = box_ops.remove_small_boxes(boxes, min_size=1e-2)
+            boxes, scores, labels = boxes[keep], scores[keep], labels[keep]
+
+            # non-maximum suppression, independently done per class
+            keep = box_ops.batched_nms(boxes, scores, labels, self.nms_thresh)
+            # keep only topk scoring predictions
+            keep = keep[: self.detections_per_img]
+            boxes, scores, labels = boxes[keep], scores[keep], labels[keep]
+
+            all_boxes.append(boxes)
+            all_scores.append(scores)
+            all_labels.append(labels)
+
+        return all_boxes, all_scores, all_labels
 
     def forward(
         self,
