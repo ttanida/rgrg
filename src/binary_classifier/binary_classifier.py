7b7ae943d4680e744f4e9ae4a1c17f038cc6c2d4
Tim Tanida <tanida.tim@googlemail.com>
2022-08-05 17:57:48 +0200

    Change implementation of forward method of full model

--- src/binary_classifier/binary_classifier.py
+++ src/binary_classifier/binary_classifier.py
@@ -19,7 +19,7 @@ class BinaryClassifier(nn.Module):
         self,
         top_region_features,  # tensor of shape [batch_size x 36 x 1024]
         class_detected,  # boolean tensor of shape [batch_size x 36], indicates if the object detector has detected the region/class or not
-        return_loss,  # boolean value that is True if we are in inference mode and want to get the regions that were selected for sentence generation by the classifier
+        return_loss,  # boolean value that is True if we need the loss (necessary for training and evaluation)
         region_has_sentence=None  # boolean tensor of shape [batch_size x 36], indicates if a region has a sentence (True) or not (False) as the ground truth
     ):
         # logits of shape [batch_size x 36]
@@ -35,54 +35,27 @@ class BinaryClassifier(nn.Module):
         if self.training:
             return loss
         else:
-            # compute selected_regions for eval
-            selected_regions = logits > 0
-            selected_regions[~class_detected] = False
-
-            if return_loss:
-                return loss, selected_regions
-            else:
-                selected_region_features = top_region_features[selected_regions]
-
-                return selected_region_features, selected_regions
-
-
-
-        # train or eval mode
-        if not return_loss:
-            # only compute loss for logits that correspond to a class that was detected
-            detected_logits = logits[class_detected]
-            detected_region_has_sentence = region_has_sentence[class_detected]
-
-            loss = self.loss_fn(detected_logits, detected_region_has_sentence)
-
-            # in train mode, only return the train loss
-            if self.training:
-                return loss
-            else:
-                # in eval model, return val loss and predictions (i.e. selected_regions)
-                # selected_regions necessary to evaluate binary classifier performance
-                # by comparing predictions to ground-truth region_has_sentence
-                #
-                # use a threshold of 0 in logit-space (i.e. 0.5 in probability-space)
-                # if a logit > 0, then it means that class/region has boolean value True and a sentence should be generated for it
-                # selected_regions is of shape [batch_size x 36] and is True for regions that should get a sentence
-                selected_regions = logits > 0
-
-                # set to False all regions that were not detected by object detector
-                # (since no detection -> no sentence generation possible)
-                selected_regions[~class_detected] = False
-
-                return loss, selected_regions
-        else:
-            # in inference mode, we need the selected regions and its features by the classifier
+            # compute selected_regions for evaluation and inference
+            # selected_regions are the predictions by the classifier, has shape [batch_size x 36] and is True for regions that should get a sentence
+            # selected_regions are needed to filter the top_region_features to those that the classifier selected for sentence generation,
+            # to evaluate the classifier during eval mode (comparing selected_regions to ground-truth region_has_sentence to compute recall, precision etc.)
+            # and to generally know which regions were selected for generation (which is needed for mapping each selected_region_feature to its corresponding class)
+            #
+            # use a threshold of 0 in logit-space (i.e. 0.5 in probability-space)
+            # if a logit > 0, then it means that class/region has boolean value True and a sentence should be generated for it
             selected_regions = logits > 0
 
             # set to False all regions that were not detected by object detector
             # (since no detection -> no sentence generation possible)
             selected_regions[~class_detected] = False
 
+            # selected_region_features are inputted into the decoder during evaluation and inference to generate the sentences
             # selected_region_features is of shape [num_regions_selected_in_batch, 1024]
             selected_region_features = top_region_features[selected_regions]
 
-            return selected_region_features, selected_regions
+            # if in eval mode
+            if return_loss:
+                return loss, selected_regions, selected_region_features
+            else:
+                # if in inference mode
+                return selected_regions, selected_region_features
