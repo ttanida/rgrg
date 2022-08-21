8a59eccf458699368ac58969539ab37aa487528a
Tim Tanida <tanida.tim@googlemail.com>
2022-08-21 15:27:54 +0200

    Fix aggregate_attention implementation

--- src/contrastive_attention/aggregate_attention.py
+++ src/contrastive_attention/aggregate_attention.py
@@ -18,11 +18,14 @@ class AggregateAttention(nn.Module):
         # shape [36 x NORMALITY_POOL_SIZE x 2048]
         self.normality_pool_image_features = torch.load(path_to_normality_pool_image_features, map_location=device)
 
-        self.wx = nn.Parameter(torch.empty(AGGREGATE_ATTENTION_NUM, 2048, 2048), requires_grad=True)
-        self.wy = nn.Parameter(torch.empty(AGGREGATE_ATTENTION_NUM, 2048, 2048), requires_grad=True)
+        NUM_REGIONS = 36
+        HIDDEN_DIM = 2048
 
-        self.wx_bias = nn.Parameter(torch.empty(2048), requires_grad=True)
-        self.wy_bias = nn.Parameter(torch.empty(2048), requires_grad=True)
+        self.wx = nn.Parameter(torch.empty(AGGREGATE_ATTENTION_NUM, NUM_REGIONS, HIDDEN_DIM, HIDDEN_DIM), requires_grad=True)
+        self.wy = nn.Parameter(torch.empty(NUM_REGIONS, HIDDEN_DIM, HIDDEN_DIM), requires_grad=True)
+
+        self.wx_bias = nn.Parameter(torch.empty(AGGREGATE_ATTENTION_NUM, NUM_REGIONS, 1, HIDDEN_DIM), requires_grad=True)
+        self.wy_bias = nn.Parameter(torch.empty(NUM_REGIONS, 1, HIDDEN_DIM), requires_grad=True)
 
         self.reset_parameters(self.wx, self.wx_bias)
         self.reset_parameters(self.wy, self.wy_bias)
@@ -36,12 +39,10 @@ class AggregateAttention(nn.Module):
 
     def forward(
         self,
-        top_region_features: torch.FloatTensor  # shape [batch_size x 36 x 2048]
+        top_region_features: torch.FloatTensor
     ):
         """
         Forward method implements equations 4 - 6 in paper.
-
-        subscripts for einsum notation:
 
         b = batch_size
         r = region_number (i.e. 36)
@@ -49,18 +50,31 @@ class AggregateAttention(nn.Module):
         d = dimensionality (i.e. 2048)
         n = normality_pool_size (most likely 100 or 1000)
         """
-        x_wx = torch.einsum("brd, add, d -> brad", top_region_features, self.wx, self.wx_bias)
-        y_wy = torch.einsum("rnd, add, d -> nrad", self.normality_pool_image_features, self.wy, self.wy_bias)
+        # top_region_features of shape [b x r x d]
+        # wx of shape [a x r x d x d]
 
-        # M are the attention weights (see equation 4 of paper)
-        M = torch.einsum("brad, nrad -> bran", x_wx, y_wy)
+        # to multiply the two, unsqueeze the necessary dimensions in top_region_features
+        # top_region_features now of shape [b x 1 x r x 1 x d]
+        top_region_features = top_region_features.unsqueeze(2).unsqueeze(1)
+
+        # x_wx of shape [b x a x r x 1 x d]
+        x_wx = torch.matmul(top_region_features, self.wx) + self.wx_bias
+
+        # normality_pool_image_features of shape [r, n, d]
+        # wy of shape [r, d, d]
+        # y_wy of shape [r, n, d]
+        y_wy = torch.matmul(self.normality_pool_image_features, self.wy) + self.wy_bias
+
+        # M are the attention weights (see equation 4 of paper) of shape [b x a x r x 1 x n]
+        M = torch.matmul(x_wx, y_wy.transpose(-1, -2))
 
         # scale attention weights
         M = M / (top_region_features.size(-1) ** 0.5)
 
         M = nn.functional.softmax(M, dim=-1)
 
-        closest_normal_region_features = torch.einsum("bran, rnd -> brad", M, self.normality_pool_image_features)
+        # closest_normal_region_features of shape [b, a, r, 1, d]
+        closest_normal_region_features = torch.matmul(M, self.normality_pool_image_features)
 
         # return the AGGREGATE_ATTENTION_NUM closest normal image features for all 36 regions of a single image
-        return closest_normal_region_features  # shape [batch_size x 36 x AGGREGATE_ATTENTION_NUM x 2048]
+        return closest_normal_region_features
