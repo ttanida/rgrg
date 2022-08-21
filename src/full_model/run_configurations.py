3d8b81fb2671421de466a253d3131850f2fb43d3
Tim Tanida <tanida.tim@googlemail.com>
2022-08-22 00:03:01 +0200

    Create new run configurations

--- src/full_model/run_configurations.py
+++ src/full_model/run_configurations.py
@@ -1,19 +1,19 @@
 # define configurations for training run
 RUN = 5
 # can be useful to add additional information to run_config.txt file
-RUN_COMMENT = """Testing out stuff."""
+RUN_COMMENT = """Train full model minus language model. Reimplemented (dynamic) contrastive attention."""
 PRETRAIN_WITHOUT_LM_MODEL = True
 IMAGE_INPUT_SIZE = 512
 NORMALITY_POOL_SIZE = 1000
 AGGREGATE_ATTENTION_NUM = 6
 PERCENTAGE_OF_TRAIN_SET_TO_USE = 1.0
-PERCENTAGE_OF_VAL_SET_TO_USE = 0.2
-BATCH_SIZE = 2
+PERCENTAGE_OF_VAL_SET_TO_USE = 0.4
+BATCH_SIZE = 16
 NUM_WORKERS = 12
 EPOCHS = 20
-LR = 1e-4
-EVALUATE_EVERY_K_STEPS = 5000  # how often to evaluate the model on the validation set and log metrics to tensorboard (additionally, model will always be evaluated at end of epoch)
-PATIENCE_LR_SCHEDULER = 5  # number of evaluations to wait for val loss to reduce before lr is reduced by 1e-1
+LR = 1e-3
+EVALUATE_EVERY_K_STEPS = 500  # how often to evaluate the model on the validation set and log metrics to tensorboard (additionally, model will always be evaluated at end of epoch)
+PATIENCE_LR_SCHEDULER = 10  # number of evaluations to wait for val loss to reduce before lr is reduced by 1e-1
 THRESHOLD_LR_SCHEDULER = 1e-3  # threshold for measuring the new optimum, to only focus on significant changes
 NUM_BEAMS = 4
 MAX_NUM_TOKENS_GENERATE = 300
