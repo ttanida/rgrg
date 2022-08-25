4502f132334747aab5396922581b71b95fb1240b
Tim Tanida <tanida.tim@googlemail.com>
2022-08-25 22:29:20 +0200

    Edit run configs

--- src/check_metrics_calculation.py
+++ src/check_metrics_calculation.py
@@ -4,25 +4,36 @@ bleu_1 = evaluate.load("bleu")
 bleu_2 = evaluate.load("bleu")
 bleu_3 = evaluate.load("bleu")
 bleu_4 = evaluate.load("bleu")
-bert_score = evaluate.load("bertscore")
 
-gen_sentences = ["I like to go swimming", "you have to pass"]
-ref_sentences = ["the ball is blue", "you have to fail"]
+# rouge = evaluate.load("rouge")
+# meteor = evaluate.load("meteor")
+
+gen_sentences = ["There is mild elevation. The heart size is normal. Lung volumes are low."]
+ref_sentences = ["There is mild elevation. Lung volumes are low. The heart size is normal."]
 
 bleu_1.add_batch(predictions=gen_sentences, references=ref_sentences)
 bleu_2.add_batch(predictions=gen_sentences, references=ref_sentences)
 bleu_3.add_batch(predictions=gen_sentences, references=ref_sentences)
 bleu_4.add_batch(predictions=gen_sentences, references=ref_sentences)
-bert_score.add_batch(predictions=gen_sentences, references=ref_sentences)
+
+# rouge.add_batch(predictions=gen_sentences, references=ref_sentences)
+# meteor.add_batch(predictions=gen_sentences, references=ref_sentences)
 
 bleu_1_result = bleu_1.compute(max_order=1)["bleu"]
 bleu_2_result = bleu_2.compute(max_order=2)["bleu"]
 bleu_3_result = bleu_3.compute(max_order=3)["bleu"]
 bleu_4_result = bleu_4.compute(max_order=4)["bleu"]
-bert_score_result = bert_score.compute(lang="en")
+
+# rouge_result = rouge.compute(rouge_types=["rougeL"], use_aggregator=False)["rougeL"][0]
+# meteor_result = meteor.compute()["meteor"]
 
 print(f"bleu_1: {bleu_1_result}")
 print(f"bleu_2: {bleu_2_result}")
 print(f"bleu_3: {bleu_3_result}")
 print(f"bleu_4: {bleu_4_result}")
-print(f"bert_score: {bert_score_result}")
+
+# print(rouge_result)
+# print(f"rouge precision: {rouge_result[0]}")
+# print(f"rouge recall: {rouge_result[1]}")
+# print(f"rouge f: {rouge_result[2]}")  # <- the one that is actually reported
+# print(f"meteor: {meteor_result}")
--- src/full_model/run_configurations.py
+++ src/full_model/run_configurations.py
@@ -1,19 +1,19 @@
 # define configurations for training run
 RUN = 5
 # can be useful to add additional information to run_config.txt file
-RUN_COMMENT = """Train full model minus language model. Reimplemented (dynamic) contrastive attention."""
+RUN_COMMENT = """(Pre-)train full model minus language model. Reimplemented (dynamic) contrastive attention. First time using gradient accumulation."""
 PRETRAIN_WITHOUT_LM_MODEL = True
 IMAGE_INPUT_SIZE = 512
-NORMALITY_POOL_SIZE = 500  # 1000
-AGGREGATE_ATTENTION_NUM = 6  # 6
-PERCENTAGE_OF_TRAIN_SET_TO_USE = 0.1  # 1.0
-PERCENTAGE_OF_VAL_SET_TO_USE = 0.005  # 0.4
-BATCH_SIZE = 8  # 16
+NORMALITY_POOL_SIZE = 500
+AGGREGATE_ATTENTION_NUM = 6
+PERCENTAGE_OF_TRAIN_SET_TO_USE = 1.0
+PERCENTAGE_OF_VAL_SET_TO_USE = 0.4
+BATCH_SIZE = 16
 EFFECTIVE_BATCH_SIZE = 64  # batch size after gradient accumulation
 NUM_WORKERS = 12
 EPOCHS = 20
 LR = 1e-3
-EVALUATE_EVERY_K_BATCHES = 100  # 640  # how often to evaluate the model on the validation set and log metrics to tensorboard (additionally, model will always be evaluated at end of epoch)
+EVALUATE_EVERY_K_BATCHES = 500  # how often to evaluate the model on the validation set and log metrics to tensorboard (additionally, model will always be evaluated at end of epoch)
 PATIENCE_LR_SCHEDULER = 10  # number of evaluations to wait for val loss to reduce before lr is reduced by 1e-1
 THRESHOLD_LR_SCHEDULER = 1e-3  # threshold for measuring the new optimum, to only focus on significant changes
 NUM_BEAMS = 4
