2314478f5778142b83c3b60e6fd61d5538647cd1
Tim Tanida <tanida.tim@googlemail.com>
2022-09-22 14:57:26 +0200

    Update setup.py doc string

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 """
 Script used to pip install src as a standalone package.
 Necessary for package imports between sibling directories.
+
+Run "pip install -e ." in the root directory (i.e. region-guided-chest-x-ray-report-generation)
+to install src.
 """
 
 from setuptools import setup, find_packages
