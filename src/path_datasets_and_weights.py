bc4d4b1ddfee3d9ec20d302e2a9db9a62f742daf
Tim Tanida <tanida.tim@googlemail.com>
2022-10-18 10:56:07 +0200

    Prepare new test set run

--- src/path_datasets_and_weights.py
+++ src/path_datasets_and_weights.py
@@ -6,13 +6,16 @@ Note that we only need the report txt files from MIMIC-CXR, which are in the fil
 https://physionet.org/content/mimic-cxr/2.0.0/.
 
 path_full_dataset specifies the path where the folder will be created (by module src/dataset/create_dataset.py) that will hold the
-train, valid, test and test-2 csv files, which will be used for training, evaluation and testing. See doc string of create_dataset.py for more information.
+train, valid, test files, which will be used for training, evaluation and testing. See doc string of create_dataset.py for more information.
+That means in my case, "/u/home/tanida/datasets/" should already exist as a directory, and the folder "dataset-with-reference-reports" would
+be created in that directory by the module.
 
 path_chexbert_weights specifies the path to the weights of the CheXbert labeler needed to extract the disease labels from the generated and reference reports.
 The weights can be downloaded here: https://github.com/stanfordmlgroup/CheXbert#checkpoint-download
 
 path_runs_* specify the directories where all the run folders (containing checkpoints, tensorboard files etc.) will be created
 when training the object detector and full model (with and without the language model), respectively.
+That means the directories specified by path_runs_* should already exist before starting the training.
 """
 
 path_chest_imagenome = "/u/home/tanida/datasets/chest-imagenome-dataset"
